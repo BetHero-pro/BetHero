516103b2185a672a37fdf2d8f258c33f90f13eb0
Thehumble_Dev <elimamnoor@gmail.com>
2023-05-24 21:51:16 +0300

    fix bug try 2

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,11 @@
 module.exports = function override(config, env) {
-  //do stuff with the webpack config...
+  config.module.rules = [
+    ...config.module.rules,
+    {
+      resolve: {
+        fallback: { https: false },
+      },
+    },
+  ];
   return config;
 };
