e6a6a050394a2e88db059fbc4089165244725256
Thehumble-Dev <105637652+Thehumble-Dev@users.noreply.github.com>
2023-05-27 16:17:03 +0300

    temporary change of domains

--- src/secrets.js
+++ src/secrets.js
@@ -2,9 +2,9 @@ const DISCORD_CLIENT_SECRET = "1Ea7GmMu62XmlJhZ8qSl-zCzg5ccfSFO";
 const DISCORD_LOCAL_LINK =
   "https://discord.com/api/oauth2/authorize?client_id=1110861504535871568&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauthenticating&response_type=code&scope=identify%20email";
 const DISCORD_PROD_LINK =
-  "https://discord.com/api/oauth2/authorize?client_id=1110861504535871568&redirect_uri=https%3A%2F%2Fbet-hero-phi.vercel.app%2Fauthenticating&response_type=code&scope=identify%20email";
+  "https://discord.com/api/oauth2/authorize?client_id=1110861504535871568&redirect_uri=https%3A%2F%2Fbet.smhm-doo.com%2Fauthenticating&response_type=code&scope=identify%20email";
 const DISCORD_LOCAL_URI = "http://localhost:3000/authenticating";
-const DISCORD_PROD_URI = "https://bet-hero-phi.vercel.app/authenticating";
+const DISCORD_PROD_URI = "https://bet.smhm-doo.com/authenticating";
 const ENV = "prod";
 
 export {
