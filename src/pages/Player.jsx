9e9c2f05c6cee160bd94560d85e526977baa9e51
devilhacker0119 <devilhacker0119@gmail.com>
2023-06-15 18:33:32 +0900

    feat: integrate order of drag-drop functionality with end point

--- src/pages/Player.jsx
+++ src/pages/Player.jsx
@@ -1,25 +1,19 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import jwt from "jsonwebtoken";
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import jwt from 'jsonwebtoken';
 // import axios from "axios";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
-import { JWT_SECRET } from "../config/env";
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import { JWT_SECRET } from '../config/env';
 // import Quest from "./Quest";
-import {
-  fetchAllQuests,
-  createQuest,
-  deleteQuest,
-  // markQuest,
-  // userAuth,
-} from "../fetches";
+import { fetchAllQuests, createQuest, deleteQuest, setOrder } from '../fetches';
 
-import "../../node_modules/font-awesome/css/font-awesome.min.css";
-import "../../node_modules/@fortawesome/fontawesome-svg-core/styles.css";
+import '../../node_modules/font-awesome/css/font-awesome.min.css';
+import '../../node_modules/@fortawesome/fontawesome-svg-core/styles.css';
 
 const Player = ({ palyerName }) => {
-  const jsonToken = localStorage.getItem("jwt");
-  const [user, setUser] = useState({ username: "", userID: "" });
-  const [newQuestion, setNewQuestion] = useState("");
+  const jsonToken = localStorage.getItem('jwt');
+  const [user, setUser] = useState({ username: '', userID: '' });
+  const [newQuestion, setNewQuestion] = useState('');
   const [questions, setQuestions] = useState([]);
   const [selectedQuestions, setSelectedQuestions] = useState([]);
   const [isQuestion, setIsQuestion] = useState(false);
@@ -27,7 +21,7 @@ const Player = ({ palyerName }) => {
   const [disabledButton, setDisabledButton] = useState(false);
   const [verification, setVerification] = useState(false);
 
-  const isSelected = (question) => {
+  const isSelected = question => {
     return selectedQuestions.includes(question);
   };
 
@@ -40,12 +34,12 @@ const Player = ({ palyerName }) => {
 
   const handleClick = () => {
     setIsQuestion(1);
-    setNewQuestion("");
+    setNewQuestion('');
   };
 
-  const handlePlayer = async (e) => {
+  const handlePlayer = async e => {
     e.preventDefault();
-    if (newQuestion !== "") {
+    if (newQuestion !== '') {
       setDisabledButton(true);
       await createQuest(user.userID, newQuestion);
       setDisabledButton(false);
@@ -54,43 +48,51 @@ const Player = ({ palyerName }) => {
     }
   };
 
-  const handleDrop = (droppedItem) => {
+  const handleDrop = async droppedItem => {
     const destinationIndex = droppedItem.destination.index;
     const sourceIndex = droppedItem.source.index;
 
-    const updatedQuestions = [...questions];
+    let updatedQuestions = [...questions];
     const [reorderedQuestion] = updatedQuestions.splice(sourceIndex, 1);
     updatedQuestions.splice(destinationIndex, 0, reorderedQuestion);
 
+    let updatedOrderData = [];
+    updatedQuestions.forEach((question, index) => {
+      question.order = index + 1;
+      updatedOrderData.push({ questID: question._id, order: index + 1 });
+    });
+
     setQuestions(updatedQuestions);
+    console.log(updatedOrderData);
+    await setOrder(updatedOrderData);
   };
 
   const navigate = useNavigate();
   useEffect(() => {
-    if (jsonToken === "") {
-      navigate("/welcome");
-    } else if (user.userID === "") {
+    if (jsonToken === '') {
+      navigate('/welcome');
+    } else if (user.userID === '') {
       try {
         const { data } = jwt.decode(jsonToken, JWT_SECRET);
         setUser({ username: data[0].userName, userID: data[0]._id });
         setVerification(true);
         toggleRefresh(!refresh);
-        document.addEventListener("keypress", (e) => {
-          if (e.key === "Enter" && isQuestion === 0) {
+        document.addEventListener('keypress', e => {
+          if (e.key === 'Enter' && isQuestion === 0) {
             setIsQuestion(1);
           }
         });
       } catch {
-        localStorage.setItem("jwt", "");
-        navigate("/welcome");
+        localStorage.setItem('jwt', '');
+        navigate('/welcome');
       }
     }
   }, []);
 
   useEffect(() => {
-    if (jsonToken === "") {
-      navigate("/welcome");
-    } else if (user.userID !== "") {
+    if (jsonToken === '') {
+      navigate('/welcome');
+    } else if (user.userID !== '') {
       fetchAllQuests(setQuestions, setSelectedQuestions, user.userID);
     }
   }, [refresh]);
@@ -104,20 +106,11 @@ const Player = ({ palyerName }) => {
               <form className="quest-form parent" onSubmit={handlePlayer}>
                 <h1>Write Quest Name</h1>
                 <div className="d-flex justify-content-center align-items-center middleDiv">
-                  <input
-                    autoFocus
-                    value={newQuestion}
-                    onChange={(e) => setNewQuestion(e.target.value)}
-                    className="custom-input"
-                  />
+                  <input autoFocus value={newQuestion} onChange={e => setNewQuestion(e.target.value)} className="custom-input" />
                 </div>
 
                 <div className="child d-flex align-items-center justify-content-center py-5">
-                  <button
-                    disabled={disabledButton}
-                    type="submit"
-                    className="btn-circle"
-                  >
+                  <button disabled={disabledButton} type="submit" className="btn-circle">
                     <i className="fa fa-check"></i>
                   </button>
                 </div>
@@ -128,45 +121,32 @@ const Player = ({ palyerName }) => {
                 <div>
                   <DragDropContext onDragEnd={handleDrop}>
                     <Droppable droppableId="list-container">
-                      {(provided) => (
-                        <div
-                          className="list-container"
-                          {...provided.droppableProps}
-                          ref={provided.innerRef}
-                        >
-                          {questions.map((question, index) => (
-                            <Draggable
-                              key={question._id}
-                              draggableId={question._id}
-                              index={index}
-                            >
-                              {(provided) => (
-                                <div
-                                  className="item-container"
-                                  ref={provided.innerRef}
-                                  {...provided.dragHandleProps}
-                                  {...provided.draggableProps}
-                                >
-                                  {
-                                    <>
-                                      <input
-                                        type="checkbox"
-                                        name={question}
-                                        disabled={false}
-                                        className="custom-check"
-                                        checked={isSelected(question)}
-                                        onChange={(e) => onChange(question, e)}
-                                        id={question._id}
-                                      />
-                                      <label className="px-5 fs-1">
-                                        {question.Quest}
-                                      </label>
-                                    </>
-                                  }
-                                </div>
-                              )}
-                            </Draggable>
-                          ))}
+                      {provided => (
+                        <div className="list-container" {...provided.droppableProps} ref={provided.innerRef}>
+                          {questions
+                            .sort((a, b) => a.order - b.order)
+                            .map((question, index) => (
+                              <Draggable key={question._id} draggableId={question._id} index={index}>
+                                {provided => (
+                                  <div className="item-container" ref={provided.innerRef} {...provided.dragHandleProps} {...provided.draggableProps}>
+                                    {
+                                      <>
+                                        <input
+                                          type="checkbox"
+                                          name={question}
+                                          disabled={false}
+                                          className="custom-check"
+                                          checked={isSelected(question)}
+                                          onChange={e => onChange(question, e)}
+                                          id={question._id}
+                                        />
+                                        <label className="px-5 fs-1">{question.Quest}</label>
+                                      </>
+                                    }
+                                  </div>
+                                )}
+                              </Draggable>
+                            ))}
                           {provided.placeholder}
                         </div>
                       )}
