24293314adc133f3a7df033b0e17afac052a4734
ali saleem(lap) <alishiekhsmart@gmail.com>
2023-06-18 12:24:35 +0500

    completed step 2 i guess

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import { useState } from "react";
-import "./App.css";
-import "./css/general.css";
-import NavComponent from "./components/NavComponent";
-import TodoList from "./components/Todo";
-import Welcome from "./pages/Welcome";
-import Login from "./pages/Login";
-import Player from "./pages/Player";
-import Quest from "./pages/Quest";
-import Intermediate from "./pages/intermediate";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from 'react';
+
+import NavComponent from './components/NavComponent';
+import TodoList from './components/Todo';
+import Welcome from './pages/Welcome';
+import Login from './pages/Login';
+import Player from './pages/Player';
+import Quest from './pages/Quest';
+import Intermediate from './pages/intermediate';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import QuestDetail from './pages/QuestDetail';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route Component={Welcome} path="/welcome" exact />
-        <Route Component={Login} path="/login" exact />
-        <Route Component={Player} path="/" exact />
-        <Route Component={Intermediate} path="/authenticating" exact />
-      </Routes>
-    </Router>
+    <>
+      <NavComponent />
+      <Router>
+        <Routes>
+          <Route Component={Welcome} path="/welcome" exact />
+          <Route Component={Login} path="/login" exact />
+          <Route Component={Player} path="/" exact />
+          <Route Component={Intermediate} path="/authenticating" exact />
+          <Route Component={QuestDetail} path="/questdetail" exact />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
