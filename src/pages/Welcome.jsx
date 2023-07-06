082b9b2f410674f4db704161de21331876b06379
bryan9605 <bdv.villan@gmail.com>
2023-07-06 16:36:48 +0800

    Home page responsive fixed

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -8,18 +8,21 @@ const Welcome = () => {
 
   return (
     <>
-      <div className="flex justify-start items-start h-screen">
+      <div className="flex justify-center lg:justify-start items-start h-screen">
         <div className="absolute inset-0  flex items-center justify-center">
           <img src="Logo.png" alt="Background" className="object-cover w-full h-full  " />
         </div>
-        <div className="relative z-10 text-center ml-16 mt-12 ">
-          <h3 className="lg:text-4xl font-bold italic">Welcome</h3>
-          <h3 className="lg:text-2xl font-bold italic">to</h3>
-          <h1 className="lg:text-8xl font-bold italic">BetHero</h1>
-          <h2 className="text-xl">(prototype version 0.1.1)</h2>
+        <div className="relative flex flex-col items-center z-10 text-center mt-16 lg:ml-20 lg:mt-16 xl:ml-28 xl:mt-20 ">
+          <h3 className="text-4xl xl:text-[48px] font-bold italic">Welcome</h3>
+          <h3 className="text-2xl xl:text-[48px] font-bold italic">to</h3>
+          <h1 className="text-[64px] sm:text-8xl xl:text-[120px] font-bold italic">BetHero</h1>
+          <h2 className="text-xl xl:text-[30px] italic  font-bold">(Prototype 0.1.1)</h2>
           <div className="mt-8"></div>
           <div className="mt-10">
-            <button onClick={handleClick} className="px-6 py-3 w-[200px] lg:text-2xl bg-blue-300  text-white font-bold rounded-md hover:bg-blue-600">
+            <button
+              onClick={handleClick}
+              className="w-[200px] xl:w-[500px] h-[64px] xl:h-[150px] flex justify-center items-center text-4xl xl:text-[80px] text-white italic font-bold px-6 py-3 bg-[#6282AE] hover:bg-[#7282AE] rounded-r-full rounded-l-full box-border"
+            >
               Enter
             </button>
           </div>
