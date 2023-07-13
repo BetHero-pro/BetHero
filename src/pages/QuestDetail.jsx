d8c2cd1664aff8b260c15fee89c7817d4afa4976
hugorydel <hugosteczkowski@gmail.com>
2023-07-13 17:18:36 -0400

    Warrior Image Addition

--- src/pages/QuestDetail.jsx
+++ src/pages/QuestDetail.jsx
@@ -1,6 +1,6 @@
 import { useState, useRef, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import { ArrowLeftIcon, CheckBadgeIcon, CheckIcon, ForwardIcon } from '@heroicons/react/24/outline';
+import { ArrowLeftIcon, CheckIcon, ForwardIcon } from '@heroicons/react/24/outline';
 import { useNavigate } from 'react-router-dom';
 import { deleteQuest, fetchAllQuests1 } from '../fetches';
 
@@ -15,7 +15,6 @@ const QuestDetail = () => {
     if (savedStartTime) {
       setStartTime(parseInt(savedStartTime));
     }
-
   }, [location.state.taskid]);
 
   useEffect(() => {
@@ -26,11 +25,10 @@ const QuestDetail = () => {
         setElapsedTime(elapsedSeconds);
       }
     }, 1000);
-    
+
     return () => {
       clearInterval(timerRef.current);
     };
-    
   }, [startTime]);
 
   const stopTimer = e => {
@@ -107,11 +105,11 @@ const QuestDetail = () => {
   function backArrowClick() {
     navigate('/');
   }
-  const skipQuest = (e) => {
+  const skipQuest = e => {
     const { quests, userid } = location.state;
     localStorage.removeItem(`timerStartTime_${location.state.currentQuest._id}`);
     if (questIndex + 1 < quests.length) {
-      stopTimer(e)
+      stopTimer(e);
       const startTime = Date.now();
       setStartTime(startTime);
       setQuestIndex(questIndex + 1);
@@ -121,17 +119,17 @@ const QuestDetail = () => {
   };
   return (
     <div className="flex flex-col bg-blue-200 w-screen h-screen">
-      <div className="flex items-center">
-        <ArrowLeftIcon
-          onClick={backArrowClick}
-          className="   bg-white border-black   cursor-pointer w-12 h-12 p-2 ml-3 shadow-xl border rounded-full "
-        />
-        <h2 className=" text-3xl text-blue-300 border bg-white rounded-xl w-[30%] mx-auto font-semibold italic text-center p-4 m-4">
-          {location.state.currentQuest.Quest}
-        </h2>
+      <div style={{ position: 'fixed', top: '40px', left: '30px' }}>
+        <ArrowLeftIcon onClick={backArrowClick} className="bg-white border-black cursor-pointer w-12 h-12 p-2 ml-3 shadow-xl border rounded-full" />
       </div>
-      <div>
-        <img className="rounded-full bg-gray-300 h-24 w-24 mx-auto mt-8" src="monster.png" alt="" />
+      <h2 className=" text-3xl text-blue-300 border bg-white rounded-xl w-[30%] mx-auto font-semibold italic text-center p-4 m-4">
+        {location.state.currentQuest.Quest}
+      </h2>
+      <div className="flex justify-center mt-8">
+        <div class="row gap-12">
+          <img style={{ transform: 'scaleX(-1)' }} className="col w-24 h-24" src="warrior.png" alt="" />
+          <img className="col rounded-full h-24 w-24" src="monster.png" alt="" />
+        </div>
       </div>
       <div className="text-center mt-4 italic text-4xl"> {formatTime(elapsedTime)}</div>
       <div className="flex justify-center mt-8">
@@ -139,8 +137,8 @@ const QuestDetail = () => {
         <img className="w-20 h-20 bg-transparent rounded p-2  cursor-pointer " src="hourglass.jpg" alt="" />
         <CheckIcon onClick={e => completeTask(e)} className="w-20 h-20 bg-green-300 rounded-full p-3  cursor-pointer" />
         <button onClick={e => skipQuest(e)} className="w-20 h-20 bg-blue-300 rounded-full p-3  cursor-pointer">
-          <ForwardIcon/>
-          </button>
+          <ForwardIcon />
+        </button>
       </div>
     </div>
   );
