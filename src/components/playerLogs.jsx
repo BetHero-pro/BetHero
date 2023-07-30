8301cac5efdac47565f2e1be355385147cdea5e6
ali saleem <alishiekhsmart@gmail.com>
2023-07-30 23:01:06 +0500

    added changes to player log

--- src/components/playerLogs.jsx
+++ src/components/playerLogs.jsx
@@ -41,6 +41,19 @@ const PlayerLogs = ({ shouldOpen, userId }) => {
         }
     };
 
+    function getImg(log) {
+        switch (log.state) {
+            case 'created newQuest':
+
+                return 'playlist.png'
+            case 'completed':
+                return 'sword.jpg'
+            case 'started':
+                return 'monster.png'
+            default:
+                break;
+        }
+    }
     return (
         <BottomSheet open={shouldOpen}
             snapPoints={({ maxHeight }) => [
@@ -62,24 +75,31 @@ const PlayerLogs = ({ shouldOpen, userId }) => {
 
 
                     const timeAgoText = timeAgo(log.createdAt);
-
+                    const imgquest = getImg(log)
+                    console.log(imgquest)
                     return (
                         <>
                             <div className='px-4 pt-3'>
                                 <div className="border border-gray-300 rounded p-3 mb-3">
-                                    <div className='flex space-x-2 '>
+                                    <div className='flex space-x-2 items-center '>
                                         <img
                                             className='w-12 h-12 rounded-full'
                                             src={avatarurl} alt="" />
                                         <div className="text-xl font-bold mb-2">{username}</div>
                                     </div>
-                                    <div className="mb-2">
+                                    <div className="mb-2 mt-2">
                                         <span>you </span>
                                         <span >{log.state}</span>
                                         <span className={log.state == 'created newQuest' ? "text-blue-600" : "text-black"}>{` ${log.Name}`}</span>
                                     </div>
 
-                                    <div className="text-green-500">{timeAgoText}</div>
+                                    <div className="flex space-x-6 items-center">
+                                        <div className="text-green-500">{timeAgoText}</div>
+                                        <div className="flex space-x-16 w-20 h-20">
+                                            <img src={imgquest} alt="" />
+                                            <img className={log.state !== 'started' ? 'hidden' : ''} src='warrior.png' alt="" />
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </>)
