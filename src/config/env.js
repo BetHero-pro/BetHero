562842436499e9937ec1303ffb06495c0abc6557
nooandpro77 <64268601+nooandpro77@users.noreply.github.com>
2023-08-02 13:27:46 +0500

    Update env.js configured to use new ip

--- src/config/env.js
+++ src/config/env.js
@@ -8,6 +8,6 @@ const DEVMODE = process.env.NODE_ENV === 'development';
 const ENV = process.env.REACT_APP_ENV ?? 'local';
 const JWT_SECRET = process.env.REACT_APP_JWT_SECRET ?? '';
 
-export const URI = DEVMODE ? 'http://localhost:5000' : 'http://34.171.209.43:5000';
+export const URI = DEVMODE ? 'http://localhost:5000' : 'http://34.72.135.229';
 
 export { DISCORD_CLIENT_SECRET, DISCORD_LOCAL_URI, DISCORD_PROD_LINK, DISCORD_PROD_URI, DISCORD_LOCAL_LINK, ENV, JWT_SECRET };
